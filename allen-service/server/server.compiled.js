ab6489f44e4f139834cded6ae698f26b66f6b7ce
allen-bui <allen.vbui@gmail.com>
2019-06-18 19:24:39 -0700

    change port numbers for elastic beanstalk

--- allen-service/server/server.compiled.js
+++ allen-service/server/server.compiled.js
@@ -8,8 +8,9 @@ var cors = require('cors');
 
 var dbConnection = require('./../db/db.js');
 
-var app = express();
-var port = process.env.port || 8081;
+var app = express(); // const port = process.env.PORT || 3001;
+
+var port = 3001;
 app.use(cors());
 app.use(express["static"](__dirname + './../dist'));
 app.use(bodyParser.urlencoded({
--- allen-service/server/server.js
+++ allen-service/server/server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dbConnection = require('./../db/db.js');
 const app = express();
-const port = process.env.port || 3001;
+// const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.use(cors());
 app.use(express.static(__dirname + './../dist'));
